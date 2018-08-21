e802d8798ad613259391b50a09cd3f584d0bfe6e
Brandon High <brandon.high@puppet.com>
2018-08-20 17:29:53 -0700

    Move apply_prep to beginning of plan

--- plans/deploy.pp
+++ plans/deploy.pp
@@ -1,12 +1,8 @@
 plan puppetbox::deploy(
   String[1] $host
 ) {
-  $puppet_installed = run_command('which puppet', $host)
-
-  unless $puppet_installed {
-    # The puppet-agent is required to do anything more interesting than executing shell scripts.
-    run_command('sudo rpm -Uvh https://yum.puppet.com/puppet5/puppet5-release-el-7.noarch.rpm && sudo yum -y install puppet-agent', $host)
-  }
+  # This should get the agent installed so we can do interesting things.
+  apply_prep($host)
 
   # Facts must be gathered manually
   run_plan(facts, nodes => $host)
@@ -33,7 +29,6 @@ plan puppetbox::deploy(
   }
 
   # Apply some Puppet module code
-  apply_prep($host)
   $apply_results = apply($host) {
     class { 'docker':
       version => 'latest',
