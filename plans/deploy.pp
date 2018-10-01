eeb6dec858259d08ce2eafab604d35044cb3d3b1
Brandon High <brandon.high@puppet.com>
2018-10-01 16:17:23 -0700

    Add various puppet git repos
    This seems super jank; is there a better way like querying the GH API to
    get the available repos and then cloning them all? (shrug)

--- plans/deploy.pp
+++ plans/deploy.pp
@@ -3,11 +3,51 @@ plan pupperbox::deploy(
   String $username = 'brandon.high',
   String $ssh_domain = 'puppet.com',
   # Override the packages installed
-  Array[String] $packages = []
+  Array[String] $packages = [],
+  Optional[String] $github_user = undef,
+  # Will be used to checkout repos over HTTPS, which ends up storing the token in
+  # plain text.
+  Optional[String] $github_token = undef
 ) {
   # This should get the agent installed so we can do interesting things.
   $nodes.apply_prep
 
+  $_homedir = "/home/${username}"
+  $_srcdir = "${_homedir}/src"
+  $_dir_mode = '0750'
+  $github_repos = {
+    'puppetlabs' => [
+      'beaker',
+      'beaker-answers',
+      'beaker-pe',
+      'chloride',
+      'enterprise_tasks',
+      'frankenbuilder',
+      'higgs',
+      'pe-installer-shim',
+      'pe-installer-vanagon',
+      'pe-modules-vanagon',
+      'pe-tasks-vanagon',
+      'peteam-statusboard',
+      'pe_acceptance_tests',
+      'puppet',
+      'puppetlabs-pe_accounts',
+      'puppetlabs-pe_concat',
+      'puppetlabs-pe_hocon',
+      'puppetlabs-pe_infrastructure',
+      'puppetlabs-pe_inifile',
+      'puppetlabs-pe_install',
+      'puppetlabs-pe_java_ks',
+      'puppetlabs-pe_manager',
+      'puppetlabs-pe_nginx',
+      'puppetlabs-pe_postgresql',
+      'puppetlabs-pe_puppet_authorization',
+      'puppetlabs-pe_razor',
+      'puppetlabs-pe_repo',
+      'puppetlabs-pe_staging',
+      'puppetlabs-puppet_enterprise',
+    ]
+  }
 
   # Some packages I like to have
   if $packages {
@@ -42,7 +82,7 @@ plan pupperbox::deploy(
       ensure     => present,
       groups     => ['wheel', 'docker'],
       managehome => true,
-      home       => "/home/${username}",
+      home       => $_homedir,
       shell      => '/usr/bin/zsh',
       require    => Class['docker'],
     }
@@ -65,6 +105,31 @@ plan pupperbox::deploy(
     }
     -> ohmyzsh::plugins { $username: plugins => ['bundler', 'colorize', 'docker', 'git', 'github', 'ruby', 'rvm', 'vi-mode'] }
     -> ohmyzsh::theme { $username: theme => 'robbyrussell' }
+
+    file { [$_srcdir]:
+      ensure => directory,
+      owner  => $username,
+      group  => $username,
+      mode   => $_dir_mode,
+    }
+
+    $github_repos.each | $org, $repos | {
+      $repos.each |$repo_name| {
+        if $github_user and $github_token {
+          notice("Checking out ${org}/${repo_name}")
+          vcsrepo { "${_srcdir}/${org}/${repo_name}":
+            ensure   => present,
+            owner    => $username,
+            provider => git,
+            source   => {
+              'origin'   => "https://${github_user}:${github_token}@github.com/${org}/${repo_name}.git",
+              'upstream' => "https://${github_user}:${github_token}@github.com/${github_user}/${repo_name}.git"
+            },
+            require  => File[$_srcdir],
+          }
+        }
+      }
+    }
   }
 
   $apply_results.each | $result | {
